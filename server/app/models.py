f91936f912f619d798521c94e5b5c439cec05118
Luca <auth@luca.cx>
2026-04-03 13:33:53 +0200

    Movie models

--- server/app/models.py
+++ server/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=256)
+    year = models.CharField(max_length=4, null=True)
+    imdb_title_id = models.CharField(max_length=20, unique=True)
+
+
+class MovieQueryHistory(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now_add=True)
